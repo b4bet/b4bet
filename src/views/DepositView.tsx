679f87a8d2cfc3c91ea098683e7778d4e72901e9
b4bet <b4betonline@gmail.com>
2026-07-30 17:13:49 +0530

    fix: remove duplicate pushState from DepositView, PaymentMethodFlow handles it

--- src/views/DepositView.tsx
+++ src/views/DepositView.tsx
@@ -1,21 +1,10 @@
-import { useEffect } from 'react';
 import type { Route } from '../components/BottomNav';
 import PaymentMethodFlow from '../components/PaymentMethodFlow';
 
 interface Props { onNavigate: (r: Route) => void; }
 
 export default function DepositView({ onNavigate }: Props) {
-  // Push a history entry so mobile back button works
-  useEffect(() => {
-    window.history.pushState({ depositFlow: true }, '');
-    const onPop = () => onNavigate('home');
-    window.addEventListener('popstate', onPop);
-    return () => window.removeEventListener('popstate', onPop);
-  }, [onNavigate]);
-
   return (
-    <>
-      <PaymentMethodFlow flow="deposit" open={true} onClose={() => onNavigate('home')} />
-    </>
+    <PaymentMethodFlow flow="deposit" open={true} onClose={() => onNavigate('home')} />
   );
 }
